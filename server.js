fc87b7c240afa79dce02b906bd023bcf0c59b90f
Brian Cama <briancama@gmail.com>
2017-03-06 15:51:01 -0800

    Remove githubAuth

--- server.js
+++ server.js
@@ -10,48 +10,48 @@ var path = require('path'),
     glob = require('glob'),
     md = require(__dirname + '/node_modules/reveal-md/node_modules/reveal.js/plugin/markdown/markdown'),
     exec = require('child_process').exec,
-    githubUtils = require('./seafewd6/app/utils/githubUtils'),
-    githubUtils2 = require('./seafewd8/app/utils/githubUtils'),
+    // githubUtils = require('./seafewd6/app/utils/githubUtils'),
+    // githubUtils2 = require('./seafewd8/app/utils/githubUtils'),
     compression = require('compression'),
-    serverStatic = require('serve-static'),
-    appConstants = require('./seafewd6/app/constants/appConstants');
+    serverStatic = require('serve-static');
+    //appConstants = require('./seafewd6/app/constants/appConstants');
 
-// github method to get auth token
-var githubOAuth = require('github-oauth')({
-  githubClient: '9ea07a0c928f3bfc6884',
-  githubSecret: '7fff2290617f3950310d487a2b4f2d312106a786',
-  baseURL: 'http://seafewd6.briancama.com',
-  loginURI: '/login',
-  callbackURI: '/callback',
-  scope: appConstants.GITHUB_API_SCOPE
-});
+// // github method to get auth token
+// var githubOAuth = require('github-oauth')({
+//   githubClient: '9ea07a0c928f3bfc6884',
+//   githubSecret: '7fff2290617f3950310d487a2b4f2d312106a786',
+//   baseURL: 'http://seafewd6.briancama.com',
+//   loginURI: '/login',
+//   callbackURI: '/callback',
+//   scope: appConstants.GITHUB_API_SCOPE
+// });
 
-githubOAuth.on('error', function(err) {
-  console.error('there was a login error', err);
-});
+// githubOAuth.on('error', function(err) {
+//   console.error('there was a login error', err);
+// });
 
-githubOAuth.on('token', function(token, serverResponse) {
-  process.stdout.write('TOKEN RECEIVED');
-  githubUtils.login(token.access_token, serverResponse);
-});
+// githubOAuth.on('token', function(token, serverResponse) {
+//   process.stdout.write('TOKEN RECEIVED');
+//   githubUtils.login(token.access_token, serverResponse);
+// });
 
-var githubOAuth2 = require('github-oauth')({
-  githubClient: '67482b98da5a857ba35b',
-  githubSecret: '4baaab441670698c87574a7bfe5a24f995444417',
-  baseURL: 'http://seafewd8.briancama.com',
-  loginURI: '/fewd8login',
-  callbackURI: '/fewd8callback',
-  scope: appConstants.GITHUB_API_SCOPE
-});
+// var githubOAuth2 = require('github-oauth')({
+//   githubClient: '67482b98da5a857ba35b',
+//   githubSecret: '4baaab441670698c87574a7bfe5a24f995444417',
+//   baseURL: 'http://seafewd8.briancama.com',
+//   loginURI: '/fewd8login',
+//   callbackURI: '/fewd8callback',
+//   scope: appConstants.GITHUB_API_SCOPE
+// });
 
-githubOAuth2.on('error', function(err) {
-  console.error('there was a login error', err);
-});
+// githubOAuth2.on('error', function(err) {
+//   console.error('there was a login error', err);
+// });
 
-githubOAuth2.on('token', function(token, serverResponse) {
-  process.stdout.write('TOKEN RECEIVED');
-  githubUtils2.login(token.access_token, serverResponse);
-});
+// githubOAuth2.on('token', function(token, serverResponse) {
+//   process.stdout.write('TOKEN RECEIVED');
+//   githubUtils2.login(token.access_token, serverResponse);
+// });
 
 
 
@@ -125,41 +125,19 @@ var render = function(res, markdown) {
 
 // server paths
 var briancama = connect();
-var seafewd = connect();
-var seafewd8 = connect();
+var seafewd12 = connect();
 
 // get reveal files from reveal-md module
 ['css', 'js', 'images', 'plugin', 'lib'].forEach(function(dir) {
-  seafewd.use('/' + dir, serverStatic(opts.revealBasePath + dir));
-  seafewd8.use('/' + dir, serverStatic(opts.revealBasePath + dir));
+  seafewd12.use('/' + dir, serverStatic(opts.revealBasePath + dir));
 });
 
 briancama.use(serverStatic(__dirname + "/dist"));
-seafewd.use(serverStatic(__dirname + "/seafewd6/dist"));
-seafewd8.use(serverStatic(__dirname + "/seafewd8/dist"));
+seafewd12.use(serverStatic(__dirname + "/seafewd12/dist"));
 
 var app = connect();
 app.use(compression());
-app.use(vhost('seafewd6.briancama.com', seafewd));
-app.use(vhost('seafewd8.briancama.com', seafewd8));
+app.use(vhost('seafewd12.briancama.com', seafewd12));
 app.use(vhost('briancama.com', briancama));
 
-http.createServer(function(req, res) {
-  if (req.url.match(/fewd8login/)) {
-    return githubOAuth2.login(req, res)
-  }
-  else if (req.url.match(/login/)) {
-    return githubOAuth.login(req, res)
-  }
-  else if (req.url.match(/fewd8callback/)) {
-    return githubOAuth2.callback(req, res)
-  }
-  else if (req.url.match(/callback/)) {
-    return githubOAuth.callback(req, res)
-  }
-  else if (req.url.match(/(\w+\.md)$/)) {
-    return renderMarkdownAsSlides(req, res)
-  } else {
-    return app(req, res)
-  }
-}).listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 80);
